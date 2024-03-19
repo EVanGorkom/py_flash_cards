9ed6e81f881e1b054ee61dffe168576fb35ff262
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-03-19 15:48:49 -0500

    Finish the number correct by category and percent by category methods and tests

--- lib/round.py
+++ lib/round.py
@@ -8,6 +8,7 @@ class Round:
         self.card_index = 0
         self.current_card = self.deck.cards[self.card_index]
         self.number_correct = 0
+        # self.percent_correct = 0
 
     def take_turn(self, guess):
         turn = Turn(guess, self.current_card)
@@ -16,4 +17,21 @@ class Round:
         self.turns.append(turn)
         self.card_index += 1
         return turn
-    
+
+    # def calc_percent_correct(self):
+    #     # pdb.set_trace()
+    #     if len(self.turns) == 0:
+    #         return 0
+    #     else:
+    #         return (self.number_correct) / (len(self.turns)) * 100
+
+    def number_correct_by_category(self, category):
+        num_correct_by_cat = 0
+        for turn in self.turns:
+            if turn.card.category == category:
+                num_correct_by_cat += 1
+        return num_correct_by_cat
+
+    def percent_correct_by_category(self, category):
+        num = ((self.number_correct_by_category(category) / len(self.turns)) * 100)
+        return float(num)
--- tests/round_test.py
+++ tests/round_test.py
@@ -56,3 +56,25 @@ def test_take_turn_sad():
     assert round.turns == [new_turn]
     assert round.card_index == 1
     assert round.number_correct == 0
+
+def test_number_correct_by_category():
+    card1 = Card("What is the capital of Alaska?", "Juneau", "Geography")
+    card2 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM")
+    card3 = Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM")
+    deck = Deck([card1, card2, card3])
+    round = Round(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert round.number_correct_by_category("Geography") == 1
+
+def test_percent_correct_by_category():
+    card1 = Card("What is the capital of Alaska?", "Juneau", "Geography")
+    card2 = Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM")
+    card3 = Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM")
+    deck = Deck([card1, card2, card3])
+    round = Round(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert round.percent_correct_by_category('Geography') == 100.00
+    turn2 = round.take_turn("Jupiter")
+    assert round.percent_correct_by_category('STEM') == 0.00
